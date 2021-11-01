d84e652c8593fc4e8dbd768a012ad5ced6b0ecf4
shivam sharma <shivam>
2021-11-01 19:22:05 +0530

    all changes done

--- pages/SchoolMassage.js
+++ pages/SchoolMassage.js
@@ -44,7 +44,7 @@ const SchoolMassage = ({ header_data }) => {
         className="flex flex-col md:flex-row w-full justify-center font-normal leading-7 
 text-base px-10 md:px-16 py-10 text-gray-600 bg-[#cbdcf8]"
       >
-        <div className="w-4/5  justify-center mb-5 flex align-middle md:w-6/12 ">
+        <div className="w-5/5  justify-center mb-5 flex align-middle md:w-6/12 ">
 
           <img className=" h-[150px] md:h-[200px] lg:h-[250px]" src={header_data?.data ? header_data?.data[0]?.logo?.data?.full_url : "https://rosemarydn.com/images/logo.png"}
           //  src="https://rosemarydn.com/images/school.PNG"
